968d5b685330c675178a61e5d433e2a2fe49ae84
Thomas Lacheheb <42994090+thomas-lacheheb@users.noreply.github.com>
2021-03-23 15:00:30 +0100

    test du webhook + tag
    test du webhook + tag

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenue, Ceci est un test du webhook !");
+        System.out.println("Bienvenue, Ceci est un test du webhook + tag !");
     }
 }
