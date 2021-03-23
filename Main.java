18f63755f3bc16b4ab5910c0c9de46fb3858e01f
Thomas Lacheheb <42994090+thomas-lacheheb@users.noreply.github.com>
2021-03-23 12:14:51 +0100

    test du webhook
    test du webhook

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenue, Webhook commit !");
+        System.out.println("Bienvenue, Ceci est un test du webhook !");
     }
 }
