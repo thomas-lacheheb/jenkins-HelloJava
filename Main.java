7e6524a0f8d4b319d0df810864fd1d6abcf02227
Thomas Lacheheb <42994090+thomas-lacheheb@users.noreply.github.com>
2021-03-23 11:50:09 +0100

    test webhook 4
    test webhook 4

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenue, test webhook 3 !");
+        System.out.println("Bienvenue, test webhook 4 !");
     }
 }
